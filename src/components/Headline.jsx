7c24d26dc61ba2b30fa46a691a99b06f555f5133
KayVeeZ <kshitijvashisth@gmail.com>
2025-04-08 20:46:48 +0100

    [tweak] final tweaks

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -3,14 +3,13 @@ import React from 'react'
 const Headline = () => {
     return (
         <>
-            <div className="text-2xl flex space-x-4 space-y-6 items-center justify-center">
+            <div className="text-2xl flex space-x-4 space-y-6">
                 <p className='text-4xl'>👋</p>
-                <div className="custom-green-bg text-white px-4 rounded font-bold">
+                <div className="bg-[#20C20E] text-white px-4 rounded-full z-100000 hover:bg-[#1A9A0B]">
                     I'M
                 </div>
                 <p className="custom-outline-white">KSHITIJ</p>
-                <p className="text-white custom-outline-green">VASHISTH</p>
-                <p className="text-[#20C20E] custom-outline-white">!</p>
+                <p className="text-white custom-outline-green">VASHISTH<span className='text-[#20C20E]'>!</span></p>
             </div>
         </>
     )
