f80b7382d82b871dd49b77decb97af30b7d24239
KayVeeZ <kshitijvashisth@gmail.com>
2025-04-09 15:36:40 +0100

    [tweak] width tweak1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="custom-green select-none w-[95%] rounded-4xl flex flex-col items-center text-center p-6 space-y-6">
+    <div className="custom-green select-none w-[90%] rounded-4xl flex flex-col items-center text-center p-6">
       
       <div className="translucent-container p-2">
       <p className="text-lg">Have a project in mind?</p>
       
       <a
         href="mailto:kshitijvashisth@gmail.com"
-        className="text-3xl sm:text-4xl inline-block mt-5 mb-4 hover:underline"
+        className="text-3xl inline-block mt-5 mb-4 hover:underline"
       >
         kshitijvashisth@gmail.com
       </a>
