dc57e1befe1eed424e403d23157b3d35c13a1435
KayVeeZ <kshitijvashisth@gmail.com>
2025-04-14 22:27:52 +0100

    [tweak] aesthetic changes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,7 +9,7 @@ const Intro = () => {
                 {/* Card */}
                 <div className="translucent-container p-6 rounded-3xl">
                  
-                    <p className="text-xl text-left">
+                    <p className="text-[18px] text-left">
                     A full-stack developer who lives and breathes code 💻🔥, obsessed with building blazing-fast ⚡, scalable 📈 applications that don’t just work—they thrill users 🎢. I geek out over clean architecture 🏗️, buttery-smooth UIs 🧈🎨, and solving gnarly technical challenges 💡🧠. When I’m not refactoring 🔄 or debugging 🐞, you’ll find me absorbing new tech 📚🤓, or engaged in technical discussions with fellow devs 👨🏫✨. Let’s collaborate and turn wild ideas into rock-solid reality 🚀—preferably with coffee ☕ in hand.
                     </p>
                 </div>
