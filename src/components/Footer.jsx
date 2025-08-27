6701b16fa00e217cc33795adb6a1b01246cfb114
KayVeeZ <kshitijvashisth@gmail.com>
2025-08-27 13:32:00 +0100

    changed name spelling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -55,7 +55,7 @@ const Footer = () => {
       </div>
 
       <div className="translucent-container w-[95%] text-[12px]">
-        <p className="text-muted-foreground">Design by: Kshitij Vashisth © 2025 </p>
+        <p className="text-muted-foreground">Design by: Kkshitij Vasshisth © 2025 </p>
         <p className="text-muted-foreground">All copyrighted properties belong to rightful owners. </p>
       </div>
 
